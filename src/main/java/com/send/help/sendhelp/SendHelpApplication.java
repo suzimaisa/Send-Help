43c3fdefeaaa5658b5d58a427e7f3e0a553e744d
Meire Lopes <jucemeirelopes@gmail.com>
2022-07-17 17:26:59 -0300

    Iniciando projeto

---
+++ src/main/java/com/send/help/sendhelp/SendHelpApplication.java
@@ -0,0 +1,13 @@
+package com.send.help.sendhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/send/help/sendhelp/SendHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.send.help.sendhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
