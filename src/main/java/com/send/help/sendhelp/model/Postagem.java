65511747358eb186095bd45e79bb4d6514e0f0f1
edson amorim <edsonlibro1@gmail.com>
2022-07-30 12:31:54 -0300

    criacao da classe postagem

--- src/main/java/com/send/help/sendhelp/model/Postagem.java
+++ src/main/java/com/send/help/sendhelp/model/Postagem.java
@@ -1,4 +1,18 @@
-package com.send.help.sendhelp.model;
-
+    package com.send.help.sendhelp.model;
+@Entity
+@Data
+@Table(name="postagem")
+@NoArgsConstructor
 public class Postagem {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    int id;
+    String titulo;
+    String descricao;
+    @OneToMany
+    Usuario autor;
+    @ManyToMany
+    Ong ong;                            
+    @ManyToOne          
+    List<Comentario> comentarios;
 }
