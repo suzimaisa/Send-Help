66cf14ae852144030d213972b61cbf9a111c75dd
Meire Lopes <jucemeirelopes@gmail.com>
2022-07-30 12:38:13 -0300

    Alteração na classe postagem

--- src/main/java/com/send/help/sendhelp/model/Postagem.java
+++ src/main/java/com/send/help/sendhelp/model/Postagem.java
@@ -1,4 +1,19 @@
-    package com.send.help.sendhelp.model;
+package com.send.help.sendhelp.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Data
 @Table(name="postagem")
